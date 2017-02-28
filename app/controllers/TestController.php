c4a78332db49c1034c6a685acc7f75f34b406f2d
vinson <laofan_7420@qq.com>
2017-02-28 23:30:14 +0800

    新增测试控制器

---
+++ app/controllers/TestController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vinosn
+ * Date: 2017/2/28
+ * Time: 23:25
+ */
+
+class TestController extends ControllerBase{
+    public function indexAction(){
+
+    }
+}
