54871a90cc728d857c09cd87aacd9538a1e9bb84
vinson <laofan_7420@qq.com>
2017-03-01 00:15:37 +0800

    的

---
+++ app/library/Wxpay/wxpay.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vinosn
+ * Date: 2017/3/1
+ * Time: 0:12
+ */
+
+ini_set('date.timezone','Asia/Shanghai');
+//error_reporting(E_ERROR);
+require_once "lib/WxPay.Api.php";
+require_once "WxPay.JsApiPay.php";
+require_once 'log.php';
+
